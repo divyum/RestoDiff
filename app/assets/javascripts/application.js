a7a3e7a382d1dfc32fd45741a53e2500eff09172
divyum <divyum@browserstack.com>
2016-08-01 03:32:33 +0530

    initial commit with typeahead search

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,49 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+$(document).ready(function(){
+
+var bloodhound = new Bloodhound({
+  datumTokenizer: function (d) {
+    return Bloodhound.tokenizers.whitespace(d.value);
+  },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+  // sends ajax request to /typeahead/%QUERY
+  // where %QUERY is user input
+  remote: {
+      url: "/typeahead/%QUERY",
+      wildcard: "%QUERY"
+    },
+  limit: 50
+});
+bloodhound.initialize();
+
+
+$('.typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1,
+},
+{
+  displayKey: 'name',
+  source: bloodhound.ttAdapter()
+});
+
+$('.typeahead').bind('typeahead:selected', function(event, datum, name) {
+  console.log(datum.location_id);
+});
+})
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+require 'net/https'
+require 'geocoder'
+
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def about
+  end
+
+  def search
+  	query = params[:q]
+  	url = URI.parse(API_URL + "search?q=#{query}")
+  	https = Net::HTTP.new(url.host,url.port)
+  	req = Net::HTTP::Get.new(url)
+  	https.use_ssl = true
+  	req["user-key"] = CONFIG["api_key"]
+  	req["Accept"] = "application/json"
+  	resp = https.request(req)
+  	content =  JSON.parse(resp.body)
+  	render json: {
+      response: content
+     }
+  end
+
+	def typeahead
+  	render json: Place.where(name: params[:query])
+	end
+
+	def typeahead
+  	render json: Place.where('name like ?', "%#{params[:query]}%")
+	end
+end
